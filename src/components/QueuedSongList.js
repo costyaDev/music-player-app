82476ea9f65bd888a3c90a2b8c42a96e4ec17137
costyaDev <ecostyaDev@gmail.com>
2020-07-05 00:30:24 +0300

    add 6 components and set Header

---
+++ src/components/QueuedSongList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QueuedSongList = () => {
+  return <div>QueuedSongList</div>;
+};
+
+export default QueuedSongList;
---
+++ src/components/SongPlayer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import QueuerdSongList from "./QueuedSongList";
+
+const SongPlayer = () => {
+  return (
+    <>
+      <div>SongPlayer</div>
+      <QueuerdSongList />
+    </>
+  );
+};
+
+export default SongPlayer;
