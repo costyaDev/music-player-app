9889fc1a08f664ea77f02d8ff28b66d3d7aa16eb
costyaDev <ecostyaDev@gmail.com>
2020-07-06 23:33:12 +0300

    adding new component queue song and @media query

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import AddSong from "./components/AddSong";
 import SongList from "./components/SongList";
 import SongPlayer from "./components/SongPlayer";
 import Header from "./components/Header";
-import { Grid } from "@material-ui/core";
+import { Grid, useMediaQuery, Hidden } from "@material-ui/core";
 
 function App() {
+  const greaterThanMD = useMediaQuery((theme) => theme.breakpoints.up("md"));
+  const greaterThanSM = useMediaQuery((theme) => theme.breakpoints.up("sm"));
   return (
     <>
-      <Header />
+      <Hidden only="xs">
+        <Header />
+      </Hidden>
       <Grid container spacing={3}>
         <Grid
           style={{
-            paddingTop: 80,
+            paddingTop: greaterThanSM ? 80 : 10,
           }}
           item
           xs={12}
@@ -22,12 +26,21 @@ function App() {
           <SongList />
         </Grid>
         <Grid
-          style={{
-            position: "fixed",
-            width: "100%",
-            right: 0,
-            top: 70,
-          }}
+          style={
+            greaterThanMD
+              ? {
+                  position: "fixed",
+                  width: "100%",
+                  right: 0,
+                  top: 70,
+                }
+              : {
+                  position: "fixed",
+                  width: "100%",
+                  left: 0,
+                  bottom: 0,
+                }
+          }
           item
           xs={12}
           md={5}
--- src/components/QueuedSongList.js
+++ src/components/QueuedSongList.js
@@ -1,7 +1,89 @@
 import React from "react";
+import {
+  Typography,
+  Avatar,
+  IconButton,
+  makeStyles,
+  useMediaQuery,
+} from "@material-ui/core";
+import { Delete } from "@material-ui/icons";
+
+const useStyle = makeStyles((theme) => ({
+  conteiner: {
+    display: "grid",
+    gridAutoFlow: "column",
+    gridTemplateColumns: "50px auto 50px",
+    gridGap: 12,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  avatar: {
+    width: 44,
+    height: 44,
+  },
+  text: {
+    textOverflow: "ellipsis",
+    overflow: "hidden",
+  },
+  songInfo: {
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+  },
+}));
 
 const QueuedSongList = () => {
-  return <div>QueuedSongList</div>;
+  const greaterThanMD = useMediaQuery((theme) => theme.breakpoints.up("md"));
+
+  const song = {
+    title: "costa",
+    artist: "epshtein",
+    thumbnail:
+      "https://s3-eu-central-1.amazonaws.com/wow-website/wp-content/uploads/2019/07/01133105/Delivery_banner11.jpg",
+  };
+
+  return (
+    greaterThanMD && (
+      <div
+        style={{
+          margin: "10px 0",
+        }}
+      >
+        <Typography color="textSecondary" variant="button">
+          QUEUE (5)
+        </Typography>
+        {Array.from({ length: 5 }, () => song).map((song, index) => (
+          <QueuedSong key={index} song={song} />
+        ))}
+      </div>
+    )
+  );
+};
+
+const QueuedSong = ({ song }) => {
+  const classes = useStyle();
+
+  const { title, artist, thumbnail } = song;
+
+  return (
+    <div className={classes.conteiner}>
+      <Avatar className={classes.avatar} src={thumbnail} alt="Song thumbnail" />
+      <div className={classes.songInfo}>
+        <Typography className={classes.text} variant="subtitle2">
+          {title}
+        </Typography>
+        <Typography
+          className={classes.text}
+          color="textSecondary"
+          variant="body2"
+        >
+          {artist}
+        </Typography>
+      </div>
+      <IconButton>
+        <Delete color="error" />
+      </IconButton>
+    </div>
+  );
 };
 
 export default QueuedSongList;
